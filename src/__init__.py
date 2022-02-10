a09293a204b69ab3363c705660e0e07984e90487
Rolando Sotelo <rola@hey.com>
2022-02-09 23:26:28 -0600

    📦 NEW: Ticket model plus templates and static

--- src/__init__.py
+++ src/__init__.py
@@ -14,6 +14,9 @@ def create_app(test_config=None):
     else:
         app.config.from_mapping(test_config)
 
+    from .models import db
+    db.init_app(app)
+
     @app.route('/')
     def index():
         return 'Hello world'
